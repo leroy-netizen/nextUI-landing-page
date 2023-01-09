a5ed013669baca375cfa3554deec0b582f7444a4
leroy-netizen <leroythomas051@gmail.com>
2023-01-09 15:06:00 +0300

    break out to components

---
+++ components/InfoCard.component.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import {Card, Text, Row, Col, Button} from "@nextui-org/react"
+import { title } from "process";
+
+interface Props{
+    title: string,
+    label: string,
+    imageUrl: string
+}
+
+const InfoCard: NextPage<Props> = (props) => {
+    // destructuring elements from the props object we are passing down
+    comst {title, label, imageUrl, studentCount }
+    return (
+        
+    );
+}
+export default InfoCard
