b4a38bc05f1aeb4a88eda7f23163b32fa105d8f6
leroy-netizen <leroythomas051@gmail.com>
2023-01-09 15:03:59 +0300

    add background image for jumbotron, set a button with a gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import type { NextPage } from "next";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
-import { Container, Navbar, Text, Button } from "@nextui-org/react";
+import { Container, Navbar, Text, Button, Grid, Col } from "@nextui-org/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,14 +26,42 @@ export default function Home() {
         <Navbar.Content>
           <Navbar.Link href="#">Login</Navbar.Link>
           <Navbar.Item>
-            <Button auto flat href="#">Sign Up </Button>
+            <Button auto flat href="#">
+              Sign Up{" "}
+            </Button>
           </Navbar.Item>
         </Navbar.Content>
       </Navbar>
-      
+
       {/* Jumbotron */}
+
+      <Grid.Container
+        justify="center"
+        css={{
+          height: "30rem",
+          backgroundImage:
+            "url(https://littlevisuals.co/images/headlights.jpg)",
+        }}
+      >
+        <Grid xs={12} sm={6} css={{ textAlign: "center" }}>
+          <Col css={{ width: "100%" }}>
+            <Text weight={"bold"} size={70} css={{ textAlign: "center" }}>
+              Your one stop shop
+            </Text>
+            <Text weight={"bold"} size={70} css={{ textAlign: "center" }}>
+              for learning Next
+            </Text>
+            <Button
+              size={"md"}
+              color={"gradient"}
+              css={{ width: "100%", marginTop: "1rem" }}
+            >
+              Give us a try
+            </Button>
+          </Col>
+        </Grid>
+      </Grid.Container>
       {/* product cards */}
     </Container>
   );
 }
-    
